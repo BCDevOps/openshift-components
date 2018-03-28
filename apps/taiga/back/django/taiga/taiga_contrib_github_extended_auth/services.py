54b43e18afd346f1dec02de9b31257ecd0136970
Shea Phillips <shea.phillips@gmail.com>
2018-03-28 13:13:05 -0700

    - reconfigure logging so we (hoopefully) see some console output

--- apps/taiga/back/django/taiga/taiga_contrib_github_extended_auth/services.py
+++ apps/taiga/back/django/taiga/taiga_contrib_github_extended_auth/services.py
@@ -1,9 +1,11 @@
 from taiga_contrib_github_auth import connector
 from taiga_contrib_github_auth import github_login_func as delegate_login_func
+import logging
 
+logger = logging.getLogger(__name__)
 
 def check_org_membership(github_id, org):
-    print("Checking membership of user with github id '{0}' in org {1}...".format(github_id, org))
+    logger.error("Checking membership of user with github id '{0}' in org {1}...".format(github_id, org))
 
     # todo implement API call to GitHub to check membership
 
@@ -11,6 +13,8 @@ def check_org_membership(github_id, org):
 
 
 def github_login_func(request):
+    logger.error("github_login_funs inside taiga_contrib_github_extended_auth....")
+
     code = request.DATA.get('code', None)
 
     email, user_info = connector.me(code)
