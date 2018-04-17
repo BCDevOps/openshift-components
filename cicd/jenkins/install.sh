df6084808b94ddcbe6a2908de017c989a531037e
Clécio Varjão <1348549+cvarjao@users.noreply.github.com>
2018-04-16 20:18:36 -0700

    Update install.sh

--- cicd/jenkins/install.sh
+++ cicd/jenkins/install.sh
@@ -3,4 +3,8 @@
 read -p "Press enter to continue installing/updating Jenkins on '$(oc project -q)'"
 
 oc import-image jenkins-2-rhel7:v3.7 --from=registry.access.redhat.com/openshift3/jenkins-2-rhel7:v3.7.42-2 --confirm
-#oc new-build jenkins-2-rhel7:v3.7~https://github.com/BCDevOps/openshift-components.git --context-dir=cicd/jenkins --name=jenkins
+oc new-build jenkins-2-rhel7:v3.7~https://github.com/BCDevOps/openshift-components.git --context-dir=cicd/jenkins --name=jenkins
+oc new-app jenkins-ephemeral -p NAMESPACE= -p JENKINS_IMAGE_STREAM_TAG=jenkins:latest --name=jenkins
+oc set resources dc/jenkins --limits=cpu=2000m,memory=4Gi --requests=cpu=1000m,memory=1Gi
+oc set volume dc/jenkins --remove --name=jenkins-data
+oc set volume dc/jenkins --add --name=jenkins-jobs  -m /var/lib/jenkins/jobs -t pvc --claim-name=jenkins-jobs --claim-class=gluster-file --claim-mode=ReadWriteOnce --claim-size=1G --overwrite
