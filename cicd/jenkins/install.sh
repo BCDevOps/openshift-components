719369637f6fddb47e0f5f87cd91b83c000e54b4
Clécio Varjão <1348549+cvarjao@users.noreply.github.com>
2018-04-16 20:07:59 -0700

    Update install.sh

--- cicd/jenkins/install.sh
+++ cicd/jenkins/install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-oc import-image jenkins-2-rhel7:v3.7 --from=registry.access.redhat.com/openshift3/jenkins-2-rhel7:v3.7.42-2 --confirm
-oc new-build jenkins-2-rhel7:v3.7~https://github.com/BCDevOps/openshift-components.git --context-dir=cicd/jenkins --name=jenkins
+read -p "Press enter to continue"
 
+oc import-image jenkins-2-rhel7:v3.7 --from=registry.access.redhat.com/openshift3/jenkins-2-rhel7:v3.7.42-2 --confirm
+#oc new-build jenkins-2-rhel7:v3.7~https://github.com/BCDevOps/openshift-components.git --context-dir=cicd/jenkins --name=jenkins
