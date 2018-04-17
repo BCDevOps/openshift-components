209942609e037cbe2f8b917b0bc0f4aef3c016e3
Clécio Varjão <1348549+cvarjao@users.noreply.github.com>
2018-04-16 20:10:42 -0700

    Update install.sh

--- cicd/jenkins/install.sh
+++ cicd/jenkins/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-read -p "Press enter to continue"
+read -p "Press enter to continue installing/updating Jenkins on '$(oc project -q)'"
 
 oc import-image jenkins-2-rhel7:v3.7 --from=registry.access.redhat.com/openshift3/jenkins-2-rhel7:v3.7.42-2 --confirm
 #oc new-build jenkins-2-rhel7:v3.7~https://github.com/BCDevOps/openshift-components.git --context-dir=cicd/jenkins --name=jenkins
